2526ad81f69f46390c7b8da43f91279756d22bd0
Tyler Dennis <ctdennis@protonmail.com>
2020-09-11 11:16:47 -0700

    System and processor implemented

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,38 @@
 #include <string>
+#include <iostream>
 
 #include "format.h"
+#include <iomanip>
 
 using std::string;
+using std::to_string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+    string formatted_time;
+    int secs, mins, hrs;
+    string secs_string, mins_string, hrs_string;
+
+    secs = seconds % 60;
+    mins = (seconds / 60) % 60;
+    hrs = seconds / 3600;
+
+    secs_string = LeadingZero(secs);
+    mins_string = LeadingZero(mins);
+    hrs_string = LeadingZero(hrs);
+
+    formatted_time = hrs_string + ":" + mins_string + ":" + secs_string;
+
+    return formatted_time;
+}
+
+string Format::LeadingZero(int timeunit) {
+    string timeunit_string;
+
+    if (timeunit < 10) {
+        timeunit_string = "0" + to_string(timeunit);
+        return timeunit_string;
+    } else {
+        timeunit_string = to_string(timeunit);
+        return to_string(timeunit);
+    }
+}
--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,29 @@
 #include "processor.h"
+#include "linux_parser.h"
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <math.h>
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+using std::string;
+using std::stof;
+
+float Processor::Utilization() { 
+    string line;
+    string cpu, user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice;
+    float passive, active, total, usage_percentage;
+    std::ifstream filestream(LinuxParser::kProcDirectory + LinuxParser::kStatFilename);
+    
+    if (filestream.is_open()) {
+        std::getline(filestream, line);
+        std::istringstream linestream(line);
+        linestream >> cpu >> user >> nice >> system >> idle >> iowait >> irq >> softirq >> steal >> guest >> guest_nice;
+    }
+
+    passive = stof(idle) + stof(iowait);
+    active = stof(user) + stof(nice) + stof(system) + stof(irq) + stof(softirq) + stof(steal);
+    total = passive + active;
+    usage_percentage = (total - passive) / total;
+
+    return roundf(usage_percentage * 100) / 100; 
+}
